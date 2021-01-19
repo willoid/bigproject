1a1c05a7f5957a88042dd3cbcc678526c9648606
WILLOID <socialguille@gmail.com>
2021-01-19 20:52:14 +0100

    changed backgrounds

--- index.js
+++ index.js
@@ -3,33 +3,42 @@
 //Animating Navbar
 
 const showNav = () => {
-  document.getElementById('navList').classList.add('showNavList');
-  document.getElementById('navBtn').classList.add('hideNavBtn') 
-}
+  document.getElementById("navList").classList.add("showNavList");
+  document.getElementById("navBtn").classList.add("hideNavBtn");
+};
 
 const hideNav = () => {
-  document.getElementById('navList').classList.remove('showNavList');
-  document.getElementById('navBtn').classList.remove('hideNavBtn') 
-}
-
+  document.getElementById("navList").classList.remove("showNavList");
+  document.getElementById("navBtn").classList.remove("hideNavBtn");
+};
 
 //handling Gallery
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
+function testClick() {
+  console.log("it works");
+}
+
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
+  console.log(n);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("galleryCard");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display= "none";}
-    slides[slideIndex-1].style.display = "block";
+  var i;
+  var slides = document.getElementsByClassName("galleryCard");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
 
 //animating the elements after animate css (https://animate.style/)
@@ -48,13 +57,13 @@ function animateCSS(element, animationName, callback) {
   node.addEventListener("animationend", handleAnimationEnd);
 }
 
-
 //animating first bg immage for desktop only
 const windowWidth = window.innerWidth;
-windowWidth > 1200 ? window.onload = function () {
-  console.log(windowWidth);
-  animateCSS("#landingPage", "fadeInDownBig")
-} : null;
+windowWidth > 1200
+  ? (window.onload = function () {
+      animateCSS("#landingPage", "fadeInDownBig");
+    })
+  : null;
 //the delay of the side elements is defined as class in the html?
 /* 
 document.getElementById("pg2whatdoido").onscroll = function() {
@@ -110,3 +119,6 @@ document.getElementById("linkedin2").onmouseover = function() {
   animateCSS("#linkedin2", "pulse");
 };
  */
+
+//the map
+
